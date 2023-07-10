8393f0f223c737d5bffb2cedf36689d1db613e94
Radek Ripa <radek.ripa@st.com>
2023-07-10 13:12:24 +0200

    working cdc on nucleo board

---
+++ USB_Device/App/usbd_cdc_if.c
@@ -0,0 +1,197 @@
+/**
+  ******************************************************************************
+  * @file    USB_Device/CDC_Standalone/USB_Device/App/usbd_cdc_if.c
+  * @author  MCD Application Team
+  * @brief   Source file for USBD CDC interface
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------ */
+#include "main.h"
+
+/* Private typedef ----------------------------------------------------------- */
+/* Private define ------------------------------------------------------------ */
+#define APP_RX_DATA_SIZE  2048
+#define APP_TX_DATA_SIZE  2048
+
+/* Private macro ------------------------------------------------------------- */
+/* Private variables --------------------------------------------------------- */
+USBD_CDC_LineCodingTypeDef LineCoding = {
+  115200,                       /* baud rate */
+  0x00,                         /* stop bits-1 */
+  0x00,                         /* parity - none */
+  0x08                          /* nb. of bits 8 */
+};
+
+/* Prescaler declaration */
+uint32_t uwPrescalerValue = 0;
+
+uint8_t UserRxBuffer[APP_RX_DATA_SIZE]; /* Received Data over USB are stored in
+                                         * this buffer */
+uint8_t UserTxBuffer[APP_TX_DATA_SIZE]; /* Received Data over UART (CDC
+                                         * interface) are stored in this buffer
+                                         */
+
+uint32_t UserTxBufPtrIn = 0;    /* Increment this pointer or roll it back to
+                                 * start address when data are received over
+                                 * USART */
+uint32_t UserTxBufPtrOut = 0;   /* Increment this pointer or roll it back to
+                                 * start address when data are sent over USB */
+
+uint8_t buf[7];
+/* USB handler declaration */
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+/* Private function prototypes ----------------------------------------------- */
+static int8_t CDC_Itf_Init(void);
+static int8_t CDC_Itf_DeInit(void);
+static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t * pbuf, uint16_t length);
+static int8_t CDC_Itf_Receive(uint8_t * pbuf, uint32_t * Len);
+static int8_t CDC_TransmitCplt(uint8_t *pbuf, uint32_t *Len, uint8_t epnum);
+
+USBD_CDC_ItfTypeDef USBD_CDC_fops = {
+  CDC_Itf_Init,
+  CDC_Itf_DeInit,
+  CDC_Itf_Control,
+  CDC_Itf_Receive,
+  CDC_TransmitCplt
+};
+
+/* Private functions --------------------------------------------------------- */
+
+/**
+  * @brief  Initializes the CDC media low layer
+  * @param  None
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Init(void)
+{
+
+
+  /* ##-5- Set Application Buffers ############################################ */
+  USBD_CDC_SetTxBuffer(&hUsbDeviceFS, UserTxBuffer, 0);
+  USBD_CDC_SetRxBuffer(&hUsbDeviceFS, UserRxBuffer);
+
+  return (USBD_OK);
+}
+
+/**
+  * @brief  CDC_Itf_DeInit
+  *         DeInitializes the CDC media low layer
+  * @param  None
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_DeInit(void)
+{
+
+  return (USBD_OK);
+}
+
+/**
+  * @brief  CDC_Itf_Control
+  *         Manage the CDC class requests
+  * @param  Cmd: Command code
+  * @param  Buf: Buffer containing command data (request parameters)
+  * @param  Len: Number of data to be sent (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t * pbuf, uint16_t length)
+{
+  switch (cmd)
+  {
+  case CDC_SEND_ENCAPSULATED_COMMAND:
+    /* Add your code here */
+    break;
+
+  case CDC_GET_ENCAPSULATED_RESPONSE:
+    /* Add your code here */
+    break;
+
+  case CDC_SET_COMM_FEATURE:
+    /* Add your code here */
+    break;
+
+  case CDC_GET_COMM_FEATURE:
+    /* Add your code here */
+    break;
+
+  case CDC_CLEAR_COMM_FEATURE:
+    /* Add your code here */
+    break;
+
+  case CDC_SET_LINE_CODING:
+	  memcpy(buf,pbuf,7);
+
+    break;
+
+  case CDC_GET_LINE_CODING:
+	  memcpy(pbuf,buf,7);
+    break;
+
+  case CDC_SET_CONTROL_LINE_STATE:
+    /* Add your code here */
+    break;
+
+  case CDC_SEND_BREAK:
+    /* Add your code here */
+    break;
+
+  default:
+    break;
+  }
+
+  return (USBD_OK);
+}
+
+
+
+
+
+/**
+  * @brief  CDC_Itf_DataRx
+  *         Data received over USB OUT endpoint are sent over CDC interface
+  *         through this function.
+  * @param  Buf: Buffer of data to be transmitted
+  * @param  Len: Number of data received (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_Itf_Receive(uint8_t * Buf, uint32_t * Len)
+{
+	  /* Initiate next USB packet transfer once UART completes transfer
+	   * (transmitting data over Tx line) */
+	  USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+  return (USBD_OK);
+}
+
+
+/**
+  * @brief  CDC_TransmitCplt
+  *         Data transmitted callback
+  *
+  *         @note
+  *         This function is IN transfer complete callback used to inform user that
+  *         the submitted Data is successfully sent over USB.
+  *
+  * @param  Buf: Buffer of data to be received
+  * @param  Len: Number of data received (in bytes)
+  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CDC_TransmitCplt(uint8_t *Buf, uint32_t *Len, uint8_t epnum)
+{
+  UNUSED(Buf);
+  UNUSED(Len);
+  UNUSED(epnum);
+
+  return (0);
+}
+
